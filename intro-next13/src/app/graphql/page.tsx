ef920d08b803baecb7ef0b50e8653ab2c1b7cde8
NicoToff <toffolo01@hotmail.com>
2023-05-21 16:10:38 +0200

    Added a GraphQL example

---
+++ intro-next13/src/app/graphql/page.tsx
@@ -0,0 +1,36 @@
+// app/graphql.tsx
+import { request, gql } from "graphql-request";
+
+const api = "https://spacex-production.up.railway.app/";
+const document = gql`
+  {
+    company {
+      name
+      ceo
+      valuation
+    }
+  }
+`;
+
+type GqlResponse = {
+  company: {
+    name: string;
+    ceo: string;
+    valuation: number;
+  };
+};
+
+export default async function GraphQLPage() {
+  const res = (await request(api, document)) as GqlResponse;
+
+  return (
+    <main>
+      <h1>GraphQL</h1>
+      <h2>{res.company.name}</h2>
+      <ul>
+        <li>CEO: {res.company.ceo}</li>
+        <li>Valuation: {res.company.valuation} $</li>
+      </ul>
+    </main>
+  );
+}
