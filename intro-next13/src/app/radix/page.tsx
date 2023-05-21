56b393aa5297cff79c326c488c41b120372ba148
NicoToff <toffolo01@hotmail.com>
2023-05-21 12:41:31 +0200

    Fixed bad version of Next; added Radix example

---
+++ intro-next13/src/app/radix/page.tsx
@@ -0,0 +1,10 @@
+import { AlertDialogDemo } from "../../../components/alert";
+// app/radix.tsx
+export default function IndexPage() {
+  return (
+    <main>
+      <h1>Radix</h1>
+      <AlertDialogDemo />
+    </main>
+  );
+}
